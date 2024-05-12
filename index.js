f1d26ee48f7ee75b08f53f7d0f62cfef19e2b931
AlievMagomed2 <152082418+AlievMagomed2@users.noreply.github.com>
2024-05-12 19:45:28 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+let menu = document.querySelector(".overlap-3")
+let menu2 = document.querySelector(".overlap-6")
+let menu3 = document.querySelector(".overlap-10")
+let menu1 = document.querySelector(".text-wrapper")
+
+menu.addEventListener("click", () => {
+    window.scrollTo(1250, 1250)
+})
+
+menu1.addEventListener("click", () => {
+    window.scrollTo(1250, 1250)
+})
+
+menu2.addEventListener("click", () => {
+    window.scrollTo(1250, 1250)
+})
+
+menu3.addEventListener("click", () => {
+    window.scrollTo(1250, 1250)
+})
+
